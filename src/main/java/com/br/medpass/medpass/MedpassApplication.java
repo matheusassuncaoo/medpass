29ae07fbbf84e7244fc35f3ede2cc1caf003f15b
Matheus Assunção da Cunha <matheus.assuncaomt18@gmail.com>
2025-07-22 22:37:24 -0400

    Initial commit

---
+++ src/main/java/com/br/medpass/medpass/MedpassApplication.java
@@ -0,0 +1,13 @@
+package com.br.medpass.medpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedpassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/medpass/medpass/MedpassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.medpass.medpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
